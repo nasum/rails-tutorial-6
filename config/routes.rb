d7580a32035f12cad318380a2a69fb1819849f21
nasum <tomato.wonder.life@gmail.com>
2021-11-28 17:18:04 +0900

    add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
+  get 'static_page/home'
+  get 'static_page/help'
+  get 'static_page/about'
   resources :microposts
   resources :users
-  root 'users#index'
 end
